70bcd2b8e05d8a2a389cbcf00b8cf14fa1b1f0ef
Brian DeHamer <brian.dehamer@hp.com>
2013-05-07 21:39:49 -0700

    Adding spec for API wrapper methods

---
+++ spec/unit/camp_out/client_spec.rb
@@ -0,0 +1,42 @@
+require 'helper'
+
+describe CampOut::Client do
+
+  let(:response) { double(:response) }
+
+  before do
+    subject.stub(get: response)
+  end
+
+  describe "#list_campgrounds" do
+
+    let(:options) { { foo: :bar } }
+
+    it "issues a get against the campgrounds resource" do
+      subject.should_receive(:get).with('campgrounds', options)
+      subject.list_campgrounds(options)
+    end
+
+    it "returns the response from the API call" do
+      subject.list_campgrounds(options).should eql response
+    end
+  end
+
+  describe "#get_campground" do
+
+    let(:contract_code) { 'cc123' }
+    let(:park_id) { 'p456' }
+
+    it "issues a get against the campground/details resource" do
+      subject.should_receive(:get).with('campground/details', {
+        contractCode: contract_code,
+        parkId: park_id
+      })
+      subject.get_campground(contract_code, park_id)
+    end
+
+    it "returns the response from the API call" do
+      subject.get_campground(contract_code, park_id).should eql response
+    end
+  end
+end
